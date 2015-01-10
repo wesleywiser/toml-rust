76f1c00977519af51c25d283034603c47f5edd10
Wesley Wiser <wwiser@gmail.com>
2015-01-09 20:17:30 -0500

    Parse full line comments
    There's a weird behavior in the windows newline test. The Lines iterator
    is defined to split on '\n' which is probably what's causing it. I don't
    think this will cause problems later.
    
    Fixes #1

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,85 @@
 #[derive(PartialEq, Show)]
 enum TomlFragment<'a> {
     Whitespace(&'a str),
+    
+    //doesn't include the leading '#'
+    Comment(&'a str),
 }
 
 #[derive(PartialEq, Show)]
 struct ParseResult<'a> {
     fragment : TomlFragment<'a>,
     remainder : &'a str,
+}
+
+fn parse_comment(s : &str) -> Option<ParseResult> {
+    if s.is_empty() {
+        return None;
+    }
+
+    if s.char_at(0) != '#' {
+        return None;
+    }
+
+    match s.lines().nth(0) {
+        Some(comment) => {
+            let fragment = TomlFragment::Comment(comment.slice_from(1));
+            let remainder = s.slice_from(comment.len());
+            
+            Some(ParseResult { fragment: fragment, remainder: remainder })
+        }
+        None => None
+    }
+}
+
+#[test]
+fn parse_comment_empty_str() {
+    assert_eq!(None, parse_comment(""));
+}
+
+#[test]
+fn parse_comment_non_comment_str() {
+    assert_eq!(None, parse_comment("test"));
+}
+
+#[test]
+fn parse_comment_only_comment_char() {
+    let fragment = TomlFragment::Comment("");
+    let remainder = "";
+
+    assert_eq!(Some(ParseResult { fragment: fragment, remainder: remainder }), parse_comment("#"));
+}
+
+#[test]
+fn parse_comment_unix_newline() {
+    let fragment = TomlFragment::Comment("");
+    let remainder = "\n";
+
+    assert_eq!(Some(ParseResult { fragment: fragment, remainder: remainder }), parse_comment("#\n"));
+}
+
+#[test]
+fn parse_comment_windows_newline() {
+    let fragment = TomlFragment::Comment("\r");
+    let remainder = "\n";
+
+    assert_eq!(Some(ParseResult { fragment: fragment, remainder: remainder }), parse_comment("#\r\n"));
+}
+
+#[test]
+fn parse_comment_text() {
+    let fragment = TomlFragment::Comment("some comment");
+    let remainder= "";
+    
+    assert_eq!(Some(ParseResult { fragment: fragment, remainder: remainder }), parse_comment("#some comment"));
+}
+
+#[test]
+fn parse_comment_content_after() {
+    let fragment = TomlFragment::Comment("test 123! :)");
+    let remainder = "\ncontent";
+
+    assert_eq!(Some(ParseResult { fragment: fragment, remainder: remainder }), parse_comment("#test 123! :)\ncontent"));
 }
 
 fn parse_whitespace(s : &str) -> Option<ParseResult> {
