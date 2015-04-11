55e0a0301d859edc9ddbc7a7c6198ef4a8e36e7c
Wesley Wiser <wwiser@gmail.com>
2015-04-11 14:49:57 -0400

    Get tests building again

--- src/tokenizer/tokenizer.rs
+++ src/tokenizer/tokenizer.rs
@@ -1,5 +1,4 @@
 use tokenizer::TomlFragment;
-//use tokenizer::TomlFragment::*;
 use super::parser_whitespace::parse_whitespace;
 use super::parser_comment::parse_comment;
 use super::parser_boolean::parse_boolean;
@@ -60,67 +59,74 @@ pub fn tokenize(s : &str) -> TokenizeResult {
     Success(tokens)
 }
 
-#[test]
-fn tokenize_error() {
-    assert_eq!(Error("Tokenize failed"), tokenize("!23121312awefa"));
-}
+#[cfg(test)]
+mod test {
+    use super::tokenize;
+    use super::TokenizeResult::{Success, Error};
+    use tokenizer::TomlFragment::*;
 
-#[test]
-fn tokenize_empty_str() {
-    assert_eq!(Success(vec![]), tokenize(""));
-} 
-
-#[test]
-fn tokenize_whitespace() {
-    let tokens = vec![Whitespace(" ")];
-    assert_eq!(Success(tokens), tokenize(" "));
-}
-
-#[test]
-fn tokenize_comment() {
-    let tokens = vec![Comment("this is a comment")];
-    assert_eq!(Success(tokens), tokenize("#this is a comment"));
-}
-
-#[test]
-fn tokenize_booleans() {
-    let tokens = vec![
-        Boolean("true"),
-        Whitespace(" "),
-        Boolean("false"),
-    ];
-    assert_eq!(Success(tokens), tokenize("true false"));
-}
-
-#[test]
-fn tokenize_whitespace_comment() {
-    let tokens = vec![
-        Whitespace("    \t"),
-        Comment("just some whitespace"),
-    ];
-    assert_eq!(Success(tokens), tokenize("    \t#just some whitespace"));
-}
-
-#[test]
-fn tokenize_positive_int_whitespace_negative_int() {
-    let tokens = vec![
-        Integer("123"),
-        Whitespace(" "),
-        Integer("-456"),
-    ];
-    assert_eq!(Success(tokens), tokenize("123 -456"));
-}
-
-#[test]
-fn tokenize_array() {
-    let tokens = vec![
-        BracketOpen,
-        Integer("1"),
-        Comma,
-        Integer("2"),
-        Comma,
-        Integer("3"),
-        BracketClose,
-    ];
-    assert_eq!(Success(tokens), tokenize("[1,2,3]"));
+    #[test]
+    fn tokenize_error() {
+        assert_eq!(Error("Tokenize failed"), tokenize("!23121312awefa"));
+    }
+    
+    #[test]
+    fn tokenize_empty_str() {
+        assert_eq!(Success(vec![]), tokenize(""));
+    } 
+    
+    #[test]
+    fn tokenize_whitespace() {
+        let tokens = vec![Whitespace(" ")];
+        assert_eq!(Success(tokens), tokenize(" "));
+    }
+    
+    #[test]
+    fn tokenize_comment() {
+        let tokens = vec![Comment("this is a comment")];
+        assert_eq!(Success(tokens), tokenize("#this is a comment"));
+    }
+    
+    #[test]
+    fn tokenize_booleans() {
+        let tokens = vec![
+            Boolean("true"),
+            Whitespace(" "),
+            Boolean("false"),
+        ];
+        assert_eq!(Success(tokens), tokenize("true false"));
+    }
+    
+    #[test]
+    fn tokenize_whitespace_comment() {
+        let tokens = vec![
+            Whitespace("    \t"),
+            Comment("just some whitespace"),
+        ];
+        assert_eq!(Success(tokens), tokenize("    \t#just some whitespace"));
+    }
+    
+    #[test]
+    fn tokenize_positive_int_whitespace_negative_int() {
+        let tokens = vec![
+            Integer("123"),
+            Whitespace(" "),
+            Integer("-456"),
+        ];
+        assert_eq!(Success(tokens), tokenize("123 -456"));
+    }
+    
+    #[test]
+    fn tokenize_array() {
+        let tokens = vec![
+            BracketOpen,
+            Integer("1"),
+            Comma,
+            Integer("2"),
+            Comma,
+            Integer("3"),
+            BracketClose,
+        ];
+        assert_eq!(Success(tokens), tokenize("[1,2,3]"));
+    }
 }
